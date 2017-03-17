be2fca1515420003b9d0df4fb076ac28a115757a
MindOverflow <toxaSmith@yahoo.com>
2017-03-17 22:12:55 +0300

    [add] Простые "накидки".

--- StringTrimmer/Program.cs
+++ StringTrimmer/Program.cs
@@ -10,6 +10,29 @@ namespace StringTrimmer
     {
         static void Main(string[] args)
         {
+            bool YesOrNot;
+
+            string fullInfo;
+            string className;
+            string methodNmae;
+
+            int lineNumber = -1;
+
+            fullInfo = "Что-то слева от скобки (( что то справа от скобки";
+
+            if (string.IsNullOrEmpty(fullInfo))
+            {
+                // Если входная строка пустая или равна null - то нечего синтаксически разбирать. 
+                YesOrNot = false;
+            }
+            else
+            {
+                YesOrNot = true;
+            }
+
+            Console.WriteLine($"Значение YesOrNot = {YesOrNot}");
+
+
         }
     }
 }
